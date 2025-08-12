10eb98b7bf20399b571dd579e460907679331baa
Bohdan <bogdannew@ukr.net>
2025-08-12 13:43:34 +0300

    Сonfigure settings for production & development

--- planetarium/management/commands/wait_for_db.py
+++ planetarium/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
 
+
 class Command(BaseCommand):
     help = "Waiting for the database to become available"
 
@@ -11,7 +12,7 @@ class Command(BaseCommand):
         db_conn = None
         while not db_conn:
             try:
-                db_conn = connections['default']
+                db_conn = connections["default"]
                 db_conn.cursor()
             except OperationalError:
                 self.stdout.write("Database not ready yet, retry in 1 second...")
